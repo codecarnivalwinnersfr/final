e6afe6dc620e1eaa05d0f26ecb70045d56bb3a83
Krishna <104525693+KRISHNA-DHARSANDIA@users.noreply.github.com>
2024-03-03 06:36:36 +0530

    a

--- app/course/[courseId]/lesson/[lessonId]/_components/CourseAttachments.tsx
+++ app/course/[courseId]/lesson/[lessonId]/_components/CourseAttachments.tsx
@@ -9,49 +9,49 @@ interface AttachmentProps {
 }
 
 const CourseAttachments = ({ attachments }: AttachmentProps) => {
-  
-const downloadAttachment = (attachment:Attachment)=>{
+
+  const downloadAttachment = (attachment: Attachment) => {
     fetch(attachment.url)
-    .then(response => response.blob())
-    .then(blob => {
-      // Create a temporary link
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(blob);
-      link.download = attachment.name;
+      .then(response => response.blob())
+      .then(blob => {
+        // Create a temporary link
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = attachment.name;
         // console.log(URL.createObjectURL(blob));
-      // Trigger a click on the link to start the download
-      link.click();
+        // Trigger a click on the link to start the download
+        link.click();
 
-      // Clean up by revoking the Object URL
-      URL.revokeObjectURL(link.href);
-    })
-    .catch(error => {
-      console.error('Error fetching file:', error);
-    });
+        // Clean up by revoking the Object URL
+        URL.revokeObjectURL(link.href);
+      })
+      .catch(error => {
+        console.error('Error fetching file:', error);
+      });
 
-    
-}  
-    return (
-    <div className="p-4">
+
+  }
+  return (
+    <div>
       {!!attachments.length && (
         <div>
-            <h4 className="font-semibold text-indigo-500">Attachments</h4>
-        
-        <div className="flex  flex-col">
-        {attachments.map((attachment) => (
-           <div className="w-fit border border-amber-400 flex items-center bg-indigo-100 rounded-lg p-3">
-            <a href={attachment.url} key={attachment.id}  className="flex items-center hover:underline line-clamp-1 text-blue-600">
-             <File className="h-4 w-4 mr-2" /> {attachment.name}
-            </a>
-            <Download className="ml-3 text-green-500 hover:text-green-700 cursor-pointer lg:h-6 lg:w-6 h-8 w-8" 
-            
-            onClick={()=> downloadAttachment(attachment)}
-            
-            />
-           </div>
+          {/* <h4 className="font-semibold text-indigo-500">Attachments</h4> */}
 
-          ))}
-        </div>
+          <div>
+            {attachments.map((attachment) => (
+              <div className="w-fit border border-amber-400 flex items-center bg-indigo-100 rounded-lg p-3 my-1">
+                <a href={attachment.url} key={attachment.id} className="flex items-center hover:underline line-clamp-1 text-blue-600">
+                  <File className="h-4 w-4 mr-2" /> {attachment.name}
+                </a>
+                <Download className="ml-3 text-green-500 hover:text-green-700 cursor-pointer lg:h-6 lg:w-6 h-8 w-8"
+
+                  onClick={() => downloadAttachment(attachment)}
+
+                />
+              </div>
+
+            ))}
+          </div>
         </div>
       )}
     </div>
--- app/course/[courseId]/lesson/[lessonId]/page.tsx
+++ app/course/[courseId]/lesson/[lessonId]/page.tsx
@@ -1,3 +1,4 @@
+
 import { getChapter } from "@/actions/getChapters";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
@@ -6,6 +7,7 @@ import LessonVideo from "./_components/LessonVideo";
 import { Button } from "@/components/ui/button";
 import { priceFormatter } from "../../../../../lib/price-formatter";
 import { Separator } from "@/components/ui/separator";
+import { useEffect } from "react";
 
 import CourseAttachments from "./_components/CourseAttachments";
 import BuyButton from "./_components/BuyButton";
@@ -16,6 +18,9 @@ import { AlertTriangle, CheckCircle, Computer } from "lucide-react";
 import Glitter from "@/components/Glitter";
 import { getProgress } from "@/actions/getProgress";
 import { Badge } from "@/components/ui/badge";
+import axios from "axios";
+
+import { v4 as uuidv4 } from 'uuid';
 
 interface lessonProps {
   params: {
@@ -24,15 +29,30 @@ interface lessonProps {
   };
 }
 
+interface Attachment {
+  id: string;
+  name: string;
+  url: string;
+  courseId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+let pdfurl: Attachment[] = [];
+let yturl: Attachment[] = [];
+let simplilearn: Attachment[] = [];
+let udamy: Attachment[] = [];
+
 const Lesson = async ({ params }: lessonProps) => {
   const { userId } = auth();
+
   if (!userId) {
     return console.log("no user");
   }
+
   const {
     chapter,
     course,
-
     attachments,
     nextChapter,
     userProgress,
@@ -43,7 +63,119 @@ const Lesson = async ({ params }: lessonProps) => {
     chapterId: params.lessonId,
   });
 
+  if (chapter != null && chapter.title != null) {
 
+    const optionspdf = {
+      method: 'GET',
+      url: 'https://google-web-search1.p.rapidapi.com/',
+      params: {
+        query: chapter.title + 'filetype:pdf',
+        limit: '2',
+        related_keywords: 'true'
+      },
+      headers: {
+        'X-RapidAPI-Key': '1179967a4dmsh66d37e6941d8960p1ce880jsnd5dddb325c5b',
+        'X-RapidAPI-Host': 'google-web-search1.p.rapidapi.com'
+      }
+    };
+
+    const optionsyt = {
+      method: 'GET',
+      url: 'https://google-web-search1.p.rapidapi.com/',
+      params: {
+        query: chapter.title + 'filetype:youtube',
+        limit: '2',
+        related_keywords: 'true'
+      },
+      headers: {
+        'X-RapidAPI-Key': '1179967a4dmsh66d37e6941d8960p1ce880jsnd5dddb325c5b',
+        'X-RapidAPI-Host': 'google-web-search1.p.rapidapi.com'
+      }
+    };
+
+    const optionsmlearn = {
+      method: 'GET',
+      url: 'https://google-web-search1.p.rapidapi.com/',
+      params: {
+        query: chapter.title + 'site:simplilearn.com',
+        limit: '3',
+        related_keywords: 'true'
+      },
+      headers: {
+        'X-RapidAPI-Key': '1179967a4dmsh66d37e6941d8960p1ce880jsnd5dddb325c5b',
+        'X-RapidAPI-Host': 'google-web-search1.p.rapidapi.com'
+      }
+    };
+
+    const optionudamy = {
+      method: 'GET',
+      url: 'https://google-web-search1.p.rapidapi.com/',
+      params: {
+        query: chapter.title + 'site:udemy.com',
+        limit: '3',
+        related_keywords: 'true'
+      },
+      headers: {
+        'X-RapidAPI-Key': '1179967a4dmsh66d37e6941d8960p1ce880jsnd5dddb325c5b',
+        'X-RapidAPI-Host': 'google-web-search1.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const respdf = await axios.request(optionspdf);
+      const resyt = await axios.request(optionsyt);
+      const ressmlearn = await axios.request(optionsmlearn);
+      const resudamy = await axios.request(optionudamy);
+
+      const urilistpdf: Attachment[] = respdf.data.results.map((result: any, index: number) => ({
+        id: `pdf_${index}`,
+        name: `${result.title.substring(0, 15)}`,
+        url: result.url,
+        courseId: uuidv4(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }))
+
+      pdfurl = urilistpdf;
+
+      const urilistyt: Attachment[] = resyt.data.results.map((result: any, index: number) => ({
+        id: `yt_${index}`,
+        name: `${result.title.substring(0, 15)}`,
+        url: result.url,
+        courseId: uuidv4(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }));
+
+      yturl = urilistyt;
+
+      const urilistsmlearn: Attachment[] = ressmlearn.data.results.map((result: any, index: number) => ({
+        id: `yt_${index}`,
+        name: `${result.title}`,
+        url: result.url,
+        courseId: uuidv4(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }));
+
+      simplilearn = urilistsmlearn;
+
+
+      const uriudmay: Attachment[] = resudamy.data.results.map((result: any, index: number) => ({
+        id: `yt_${index}`,
+        name: `${result.title}`,
+        url: result.url,
+        courseId: uuidv4(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }));
+
+      udamy = uriudmay;
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   if (!chapter || !course) {
     return redirect("/");
@@ -52,35 +184,32 @@ const Lesson = async ({ params }: lessonProps) => {
   const isFree = !chapter.isFree;
   const userProgressCount = await getProgress(userId, params.courseId);
 
-
   return (
     <div>
-      { userProgressCount === 100 ? (
+      {userProgressCount === 100 ? (
         <Alert className="bg-green-500">
           <div className="flex items-center space-x-2">
             <CheckCircle size={16} className="text-white" />
-
             <AlertDescription className="text-white">
               Congratulation 🥳 This course is completed .
             </AlertDescription>
             <Glitter isConfettiActive={userProgressCount === 100} />
           </div>
         </Alert>
-      ): <></>}
-       
+      ) : <></>}
 
-      {isLocked && (
+
+      {/* {isLocked && (
         <Alert>
           <div className="flex items-center space-x-2">
             <AlertTriangle size={16} className="text-blue-700" />
-
             <AlertDescription>
               This Course is not Purchased. It will not be visible in the
               Progress Section.
             </AlertDescription>
           </div>
         </Alert>
-      )}
+      )} */}
 
       <div>
         <div className="p-4">
@@ -100,7 +229,7 @@ const Lesson = async ({ params }: lessonProps) => {
             {chapter.title}
           </h3>
 
-          {isLocked && isFree ? (
+          {/* {isLocked && isFree ? (
             <BuyButton
               isLocked={isLocked}
               coursePrice={course.price!}
@@ -124,21 +253,53 @@ const Lesson = async ({ params }: lessonProps) => {
                 courseId={params.courseId}
               />
             </>
-          )}
-          
+          )} */}
+
         </div>
 
-    {!isLocked &&
-         ( <div className="m-5 mb-2 mt-0">
-        <span className="text-slate-800 text-sm font-bold"> Chapter Status :</span>  { userProgress?.isCompleted ? (
-          <Badge variant="secondary">Completed</Badge>
-         ): <Badge variant="destructive">Not completed</Badge>}
-           </div>)
-    }
-      
+        {/* {!isLocked &&
+          (<div className="m-5 mb-2 mt-0">
+            <span className="text-slate-800 text-sm font-bold"> Chapter Status :</span>  {userProgress?.isCompleted ? (
+              <Badge variant="secondary">Completed</Badge>
+            ) : <Badge variant="destructive">Not completed</Badge>}
+          </div>)
+        } */}
+
         <Separator />
-        <div>
-          <CourseAttachments attachments={attachments} />
+        <div className="flex flex-row">
+          {attachments && (
+            <div className="p-4">
+              <h4 className="font-semibold text-indigo-500">Attachments</h4>
+              <CourseAttachments attachments={attachments} />
+            </div>
+          )}
+          {pdfurl && (
+            <div className="p-4">
+              <h4 className="font-semibold text-indigo-500">PDF</h4>
+              <CourseAttachments attachments={pdfurl} />
+            </div>
+          )}
+          {yturl && (
+            <div className="p-4">
+              <h4 className="font-semibold text-indigo-500">You Tube</h4>
+              <CourseAttachments attachments={yturl} />
+            </div>
+          )}
+        </div>
+        <Separator />
+        <div className="flex flex-row">
+          {simplilearn && (
+            <div className="p-4">
+              <h4 className="font-semibold text-indigo-500">Simplilearn</h4>
+              <CourseAttachments attachments={simplilearn} />
+            </div>
+          )}
+           {udamy && (
+            <div className="p-4">
+              <h4 className="font-semibold text-indigo-500">Udamy</h4>
+              <CourseAttachments attachments={udamy} />
+            </div>
+          )}
         </div>
       </div>
     </div>
